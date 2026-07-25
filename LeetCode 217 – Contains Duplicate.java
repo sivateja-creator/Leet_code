d6a4d3be52924adeb240fa75de37378dfbbabc0f
Padibandla SivaTeja <chowdaryshiva779@gmail.com>
2026-07-25 07:00:25 +0530

    Implement containsDuplicate method using HashSet

---
+++ LeetCode 217 – Contains Duplicate.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int n : nums)
+            if (!set.add(n))
+                return true;
+        return false;
+    }
+}
