52cfe239a29de9daecefffeb471e6c730c54762c
Padibandla SivaTeja <chowdaryshiva779@gmail.com>
2026-09-13 07:33:34 +0530

    Create LeetCode 1399 — Count Largest Group.java

---
+++ LeetCode 1399 — Count Largest Group.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countLargestGroup(int n) {
+        int[] count = new int[37];
+        for (int i = 1; i <= n; i++) {
+            int x = i, sum = 0;
+            while (x > 0) { sum += x % 10; x /= 10; }
+            count[sum]++;
+        }
+        int max = 0, ans = 0;
+        for (int x : count) max = Math.max(max, x);
+        for (int x : count) if (x == max) ans++;
+        return ans;
+    }
+}
